f67f244c8b437361965143396e3c745fad2c8684
Hengll <heeric891120@gmail.com>
2024-11-22 10:22:24 +0800

    大致完成

--- templates/template.js
+++ templates/template.js
@@ -7,40 +7,54 @@ export default () => {
       layout: 'vertical',
       contents: [
         {
-          type: 'text',
-          text: '新北市泰山區',
-          color: '#FFFFFF',
-          align: 'center',
-          size: '3xl',
-          gravity: 'center',
-          weight: 'bold'
+          type: 'image',
+          url: 'https://img.freepik.com/free-vector/beautiful-night-sky-starry-banner-stunning-display-universe_1017-50560.jpg?t=st=1732237717~exp=1732241317~hmac=a159c45f6cd26dbb19dea3ccf3ea08e96aa2bf2d4493e9ea3dda7442b012bad6&w=1380',
+          size: 'full',
+          aspectMode: 'cover',
+          aspectRatio: '1.5:1'
         },
         {
-          type: 'separator'
-        },
-        {
-          type: 'text',
-          color: '#FFFFFF',
-          align: 'center',
-          size: 'lg',
-          gravity: 'center',
-          text: '2024-11-21',
-          margin: 'lg'
-        },
-        {
-          type: 'text',
-          color: '#FFFFFF',
-          align: 'center',
-          size: 'lg',
-          gravity: 'center',
-          text: '白天(06:00~18:00)',
-          margin: 'lg'
+          type: 'box',
+          layout: 'vertical',
+          contents: [
+            {
+              type: 'text',
+              text: '新北市板橋區',
+              color: '#FFFFFF',
+              align: 'center',
+              size: '3xl',
+              gravity: 'center',
+              weight: 'bold'
+            },
+            {
+              type: 'separator'
+            },
+            {
+              type: 'text',
+              color: '#FFFFFF',
+              align: 'center',
+              size: 'lg',
+              gravity: 'center',
+              text: '2024-11-22',
+              margin: 'lg'
+            },
+            {
+              type: 'text',
+              color: '#FFFFFF',
+              align: 'center',
+              size: 'lg',
+              gravity: 'center',
+              text: '(晚上)18:00~06:00',
+              margin: 'lg'
+            }
+          ],
+          position: 'absolute',
+          offsetStart: 'xl',
+          offsetTop: 'xl'
         }
       ],
       backgroundColor: '#27ACB2',
-      paddingTop: '19px',
-      paddingAll: '12px',
-      paddingBottom: '16px',
+      paddingAll: '0px',
       background: {
         type: 'linearGradient',
         angle: '45deg',
@@ -59,14 +73,14 @@ export default () => {
             {
               type: 'text',
               text: '陰短暫雨',
-              color: '#000000',
+              color: '#FF0000',
               size: 'sm',
               wrap: true,
               flex: 0
             },
             {
               type: 'image',
-              url: 'https://www.cwa.gov.tw/V8/assets/img/weather_icons/weathers/svg_icon/day/08.svg',
+              url: 'https://raw.githubusercontent.com/Hengll/js_weatherForecast_linebot/refs/heads/main/weatherIcon/night/png/11.png',
               size: '18px',
               flex: 0,
               margin: '2px'
@@ -80,8 +94,8 @@ export default () => {
           contents: [
             {
               type: 'text',
-              text: '降雨機率 60%\n溫度攝氏19至20度\n稍有寒意至舒適\n東北風 風速<= 1級(每秒2公尺)\n相對濕度88%\n',
-              color: '#000000',
+              text: '降雨機率 30%\n溫度攝氏18至19度\n稍有寒意\n偏東風 風速3級(每秒4公尺)\n相對濕度81%\n',
+              color: '#0000FF',
               size: 'sm',
               wrap: true
             }
